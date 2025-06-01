bfb2146e437b736c09073ce3cb3815c90cdf37e5
an-12195 <165748978+an-12195@users.noreply.github.com>
2025-06-01 11:15:29 +0530

    auth 1.5 done

--- sen-app/routes/auth.js
+++ sen-app/routes/auth.js
@@ -1,50 +1,63 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
-const {isAdmin}=require('../middleware/auth'
+const { isAdmin } = require('../middleware/auth'
 
 )
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.render('loginpage');
 });
 
-router.post('/',(req,res)=>{
-    const {uname:username,psw:password}=req.body;
+router.post('/', (req, res) => {
+    const { uname: username, psw: password } = req.body;
 
-    const q='SELECT * FROM users WHERE username=? AND password=?'
+    const q = 'SELECT * FROM users WHERE username=? AND password=?'
 
-    db.query(q,[username,password],(err,results)=>{
-        if(err){
+    db.query(q, [username, password], (err, results) => {
+        if (err) {
             console.error(err);
             return res.status(500).send('Database error');
         }
-        if (results.length===0){
-            return res.render('loginpage',{error:"Invalid Credentials"});
-        }
-        
-        const user=results[0];
-
-        req.session.user={
-            id:user.id,
-            username:user.username,
-            role:user.username==='admin'?'admin':'user',
-            profile_pic:user.profile_pic
+        if (results.length === 0) {
+            return res.render('loginpage', { error: "Invalid Credentials" });
         }
 
-        if(user.role==='admin'){
-            return res.redirect('/admin/dashboard')  
+        const user = results[0];
+
+        req.session.user = {
+            id: user.id,
+            username: user.username,
+            role: user.username === 'admin' ? 'admin' : 'user',
+            profile_pic: user.profile_pic
+        }
+
+        if (req.session.user.role === 'admin') {
+            return res.redirect('/admin/dashboard')
         } else {
             return res.redirect('/users/home');
         }
-    })});
+    })
+});
 
-router.get('/admin/dashboard',isAdmin,(req,res)=>{
+router.get('/admin/dashboard', isAdmin, (req, res) => {
     return res.render('admin/dashboard', {
-    layout: 'partials/bootstrap',
-    title: 'Admin Dashboard',
-  });
-})
-router.get('/users/home',(req,res)=>{
-    return res.render('home',{layout:false,user:req.session.user})
-})
-module.exports=router;
+        layout: 'partials/bootstrap',
+        title: 'Admin Dashboard',
+    });
+});
+
+router.get('/users/home', (req, res) => {
+    return res.render('home', { layout: false, user: req.session.user })
+});
+
+router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error('logout error: ', err);
+            return res.status(500).send("Could not log out.");
+        }
+        res.clearCookie('connect.sid');
+        res.redirect('/');
+    });
+});
+module.exports = router;
